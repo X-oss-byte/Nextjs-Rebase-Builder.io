94cf3f0a828af90bdf173144cc9add1d9c084f4d
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-10 08:39:04 +0100

    Initialize pcm-plugin-fix

---
+++ packages/sdks/src/components/render-blocks.lite.tsx
@@ -0,0 +1,70 @@
+import { isEditing } from '../functions/is-editing.js';
+import { BuilderBlock } from '../types/builder-block.js';
+import RenderBlock from './render-block/render-block.lite';
+import { Show, useState } from '@builder.io/mitosis';
+
+export type RenderBlockProps = {
+  blocks?: BuilderBlock[];
+  parent?: string;
+  path?: string;
+};
+
+export default function RenderBlocks(props: RenderBlockProps) {
+  const state = useState({
+    get className() {
+      return 'builder-blocks' + (!props.blocks?.length ? ' no-blocks' : '');
+    },
+    onClick() {
+      if (isEditing() && !props.blocks?.length) {
+        window.parent?.postMessage(
+          {
+            type: 'builder.clickEmptyBlocks',
+            data: {
+              parentElementId: props.parent,
+              dataPath: props.path,
+            },
+          },
+          '*'
+        );
+      }
+    },
+    onMouseEnter() {
+      if (isEditing() && !props.blocks?.length) {
+        window.parent?.postMessage(
+          {
+            type: 'builder.hoverEmptyBlocks',
+            data: {
+              parentElementId: props.parent,
+              dataPath: props.path,
+            },
+          },
+          '*'
+        );
+      }
+    },
+  });
+
+  return (
+    <div
+      className={state.className}
+      builder-path={props.path}
+      builder-parent-id={props.parent}
+      dataSet={{
+        class: state.className,
+      }}
+      css={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'stretch',
+      }}
+      onClick={(event) => state.onClick()}
+      onMouseEnter={(event) => state.onMouseEnter()}
+    >
+      <Show when={props.blocks}>
+        {props.blocks?.map((block) => (
+          <RenderBlock key={block.id} block={block} />
+        ))}
+      </Show>
+    </div>
+  );
+}
---
+++ packages/sdks/src/context/builder.context.lite.ts
@@ -0,0 +1,27 @@
+import { BuilderContent } from '../types/builder-content.js';
+import { ComponentInfo } from '../types/components.js';
+import { Dictionary, Nullable } from '../types/typescript.js';
+import { createContext } from '@builder.io/mitosis';
+
+export interface RegisteredComponent {
+  component: any;
+  info: ComponentInfo;
+}
+
+export type RegisteredComponents = Dictionary<RegisteredComponent>;
+
+interface BuilderContext {
+  content: Nullable<BuilderContent>;
+  context: Record<string, unknown>;
+  state: Record<string, unknown>;
+  apiKey: string | null;
+  registeredComponents: RegisteredComponents;
+}
+
+export default createContext<BuilderContext>({
+  content: null,
+  context: {},
+  state: {},
+  apiKey: null,
+  registeredComponents: {},
+});
